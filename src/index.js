b226272667ca5d30f7c1f9b97eae8f2e3224fdf9
fabiolayerim <fabiolayerim@hotmail.com>
2023-02-15 01:46:34 -0400

    clean

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Materials from "routes/materials/index";
 import Clothes from "routes/clothes/index";
