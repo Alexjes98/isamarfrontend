d86caad7a226f685e1588e1210e0fcb58790edd9
Alejandro Lopez <alexjes6543@gmail.com>
2023-03-05 18:12:20 -0400

    add register validation and validator function

--- src/routes/register/index.jsx
+++ src/routes/register/index.jsx
@@ -11,7 +11,13 @@ import {
 } from "react-bootstrap";
 import Logo from "assets/logo.png";
 
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import generalAlert from "../../components/general_alert"
+import { isEmpty, notPhoneNumber } from "../../utils/validator"
+
 export default function Register() {
+  const alertObject = { message: "Hay campos incorrectos", show: false, icon: faExclamationTriangle, variant: "warning" }
+  const [alertState, setAlertState] = useState(alertObject);
   const [state, setState] = useState({
     nombre: "",
     apellido: "",
@@ -25,10 +31,30 @@ export default function Register() {
     const value = target.value;
     const name = target.name;
     setState({ ...state, [name]: value });
+    alertObject.show = false;
+    setAlertState(alertObject);
   };
+
+  const invalidInput = () => {
+    if(isEmpty(state.nombre) || isEmpty(state.apellido) || isEmpty(state.dni) || isEmpty(state.password) || isEmpty(state.telefono)){
+      alertObject.show = true
+      alertObject.message = "Hay campos sin llenar"
+      alertObject.variant = "danger"
+      setAlertState(alertObject)
+      return true
+    }
+    if(notPhoneNumber(state.telefono)){      
+      alertObject.show = true
+      alertObject.message = "No es un numero de telefono valido"
+      alertObject.variant = "danger"
+      setAlertState(alertObject)
+    }    
+    return false
+  }
 
   const handleRegister = async () => {
     try {
+      if(invalidInput()) return;
       const pream = {
         method: "POST",
         headers: {
@@ -40,9 +66,14 @@ export default function Register() {
       const url = `${process.env.REACT_APP_API_URL}/register/`;
 
       const resp = await fetch(url, pream);
+      const jsonResp = await resp.json();
       if (resp.ok) {
         setState({ ...state, redirect: true });
       } else {
+        alertObject.show =true;
+        alertObject.message = jsonResp.error;
+        alertObject.variant = "danger"
+        setAlertState(alertObject);
       }
     } catch (e) {
       console.log(e);
@@ -56,6 +87,7 @@ export default function Register() {
       <Container>
         <Row className="mt-4 d-flex justify-content-center">
           <Col sm={6}>
+          {generalAlert({ show: alertState.show, variant: alertState.variant, message: alertState.message, icon: alertState.icon })}
             <Card body>
               <Row>
                 <Col className="text-center">
@@ -119,7 +151,7 @@ export default function Register() {
                   <Form.Group>
                     <Form.Label>Teléfono</Form.Label>
                     <Form.Control
-                      type="text"
+                      type="tel"
                       placeholder="Ingrese Teléfono"
                       name="telefono"
                       onChange={handleChange}
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,3 +1,8 @@
 export const isEmpty = (string) => {
     return string === ""; 
 }
+
+export const notPhoneNumber = (string) => {
+    const regex = /(?=.*?\d{3}( |-|.)?\d{4})((?:\+?(?:1)(?:\1|\s*?))?(?:(?:\d{3}\s*?)|(?:\((?:\d{3})\)\s*?))\1?(?:\d{3})\1?(?:\d{4})(?:\s*?(?:#|(?:ext\.?))(?:\d{1,5}))?)\b/gi;
+    return !regex.test(string);
+}
